20149a2a06592bfb35442821ed31e7d01e9f6123
nomu <@MSI>
2025-09-03 19:17:06 +0900

    滑らかに消えていく処理の追加、値をミスるとバグる可能性のためassert使用

--- class/player/Drawing.cpp
+++ class/player/Drawing.cpp
@@ -65,31 +65,63 @@ void Drawing::Update(bool isDragging)
 				recordingTimer_ -= kRecordingTime;
 				const Vector2 mousePosition = Mouse::GetPosition();
 				const Vector2 preMousePosition = points_[points_.size() - 1];
-
-				// スプライト
-				// 位置
-				const Vector2 spritePosition = (mousePosition + preMousePosition) / 2.0f;
-				// Z座標（描画順）
-				const float kPositionZ = 0.0f;
-				sprites_[sprieIndex_].worldTF.translation = { spritePosition.x, spritePosition .y, kPositionZ };
-				// 回転
-				const Vector2 kDir2D = (mousePosition - preMousePosition).Normalize();
-				const Vector3 kDir3D = { kDir2D.x, kDir2D.y, 0.0f};
-				sprites_[sprieIndex_].worldTF.rotation = Quaternion::ConvertFromTo(Vector3{0.0f,1.0f,0.0f}, kDir3D);
-				// 大きさ
 				const float kLength = (mousePosition - preMousePosition).Length();
-				sprites_[sprieIndex_].worldTF.scale.y = (kLength / kSpriteSize_.y) + 0.1f;
 
-				// アクティブに
-				sprites_[sprieIndex_].isActive = true;
-				++sprieIndex_;
-				if (sprieIndex_ == kSpriteNum_) {
-					sprieIndex_ = 0;
+				// 長さ10未満なのでリターン
+				if (kLength < 10.0f) {
+					return;
 				}
 
 				// 現在の位置を保存
 				points_.push_back(mousePosition);
+				// スプライト
+				SpriteActive(preMousePosition, mousePosition);
+
+				// 線の長さ
+				lineLength_ += kLength;
+				// 線の長さ最大
+				const float kLengthMax = 1000.0f;
+				// 差分
+				float difference = lineLength_ - kLengthMax;
+
+				while (difference > 0.0f) {
+
+					lineLength_ = kLengthMax;
+
+					// 線は一つ
+					if (points_.size() <= 2) {
+						points_[0] = (points_[1] - points_[0]).Normalize() * kLengthMax + points_[1];
+						difference = 0.0f;
+					}
+					else {
+						difference = difference - (points_[0] - points_[1]).Length();
+
+						// 0より小さい
+						if (difference < 0.0f) {
+							points_[0] = (points_[1] - points_[0]).Normalize() * difference + points_[1];
+						}
+						else {
+							for (size_t i = 0; i < points_.size() - 1; ++i) {
+								points_[i] = points_[i + 1];
+							}
+							points_.pop_back();
+						}
+					}
+
+					// スプライト
+					sprieIndex_ = 0;
+					for (size_t i = 0; i < points_.size() - 1; ++i) {
+						// スプライト
+						SpriteActive(points_[i], points_[i + 1]);
+					}
+					for (size_t i = sprieIndex_; i < kSpriteNum_; ++i) {
+						sprites_[i].isActive = false;
+					}
+
+				}
+			
 			}
+
 		}
 		// これから線を書く
 		else {
@@ -98,6 +130,32 @@ void Drawing::Update(bool isDragging)
 			// アクティブ
 			isActive_ = true;
 		}
+	}
+
+}
+
+void Drawing::SpriteActive(const LWP::Math::Vector2& point0, const LWP::Math::Vector2& point1)
+{
+
+	// 位置
+	const Vector2 spritePosition = (point1 + point0) / 2.0f;
+	// Z座標（描画順）
+	const float kPositionZ = 0.0f;
+	sprites_[sprieIndex_].worldTF.translation = { spritePosition.x, spritePosition.y, kPositionZ };
+	// 回転
+	const Vector2 kDir2D = (point1 - point0).Normalize();
+	const Vector3 kDir3D = { kDir2D.x, kDir2D.y, 0.0f };
+	sprites_[sprieIndex_].worldTF.rotation = Quaternion::ConvertFromTo(Vector3{ 0.0f,1.0f,0.0f }, kDir3D);
+	// 大きさ
+	const float kLength = (point1 - point0).Length();
+	sprites_[sprieIndex_].worldTF.scale.y = (kLength / kSpriteSize_.y) + 0.1f;
+
+	// アクティブに
+	sprites_[sprieIndex_].isActive = true;
+	++sprieIndex_;
+	if (sprieIndex_ == kSpriteNum_) {
+		sprieIndex_ = 0;
+		assert(0);
 	}
 
 }
--- class/player/Drawing.h
+++ class/player/Drawing.h
@@ -38,6 +38,15 @@ public: // メンバ関数
 	/// <param name="isDragging">ドラッグ中か</param>
 	void Update(bool isDragging);
 
+private: // メンバ関数
+	
+	/// <summary>
+	/// スプライト起動
+	/// </summary>
+	/// <param name="point0">点0</param>
+	/// <param name="point1">点1</param>
+	void SpriteActive(const LWP::Math::Vector2& point0, const LWP::Math::Vector2& point1);
+
 private: // メンバ変数
 
 	// 点
@@ -46,6 +55,7 @@ private: // メンバ変数
 	// スプライト
 	std::array<LWP::Primitive::NormalSprite, kSpriteNum_> sprites_;
 
+	// スプライト番号
 	size_t sprieIndex_;
 
 	// 動いてる
@@ -53,6 +63,9 @@ private: // メンバ変数
 
 	// 記録タイミングタイマー
 	float recordingTimer_;
+
+	// 線の長さ
+	float lineLength_;
 
 };
 
