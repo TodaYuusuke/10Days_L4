37252e54952da0d0d8c4528f6219be99b9b3f095
hamuDesctop <shiki13275713@gmail.com>
2025-09-07 22:55:08 +0900

    微々たるコードのリファクタリング

--- class/boss/normalBoss/NormalBossStateManager.cpp
+++ class/boss/normalBoss/NormalBossStateManager.cpp
@@ -6,19 +6,19 @@ NormalBossStateManager::NormalBossStateManager() {
 }
 
 uint8_t NormalBossStateManager::Update(const uint8_t currentType) {
-	ImGui::Text("currentType %u", static_cast<unsigned int>(currentType));
+	//ImGui::Text("currentType %u", static_cast<unsigned int>(currentType));
 	// 変更がなければ何もなし
 	if (!isStateChange_) { return currentType; }
 	isStateChange_ = false;
 
 	// 以下仮
-	if (currentType == static_cast<uint8_t>(NormalBossStateType::Move)) {
-		return static_cast<uint8_t>(NormalBossStateType::Idle);
+	if (currentType == StateType::ToUInt8(NormalBossStateType::Move)) {
+		return StateType::ToUInt8(NormalBossStateType::Idle);
 	}
-	else if (currentType == static_cast<uint8_t>(NormalBossStateType::Idle)) {
+	else if (currentType == StateType::ToUInt8(NormalBossStateType::Idle)) {
 		// 移動する場所を雑に設定
 		targetPosition_.x = LWP::Utility::Random::GenerateFloat(0.0f, 1280.0f);
 		targetPosition_.y = LWP::Utility::Random::GenerateFloat(0.0f, 720.0f);
-		return static_cast<uint8_t>(NormalBossStateType::Move);
+		return StateType::ToUInt8(NormalBossStateType::Move);
 	}
 }
