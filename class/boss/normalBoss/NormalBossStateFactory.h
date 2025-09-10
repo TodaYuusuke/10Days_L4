5756ccb088c96d0445262c6cb11b18d848ddc756
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2025-09-10 17:21:02 +0900

    弾のtextureを更新

--- class/boss/normalBoss/NormalBossStateFactory.h
+++ class/boss/normalBoss/NormalBossStateFactory.h
@@ -7,11 +7,14 @@
 
 class NormalBossStateManager;
 class EnemyBullet;
+class MinionManager;
 
 namespace StateFactory::NormalBoss {
 	/// <summary>
 	/// 状態作成関数
 	/// </summary>
 	/// <returns>状態一覧</returns>
-	std::unordered_map<uint8_t, std::unique_ptr<IEnemyState>> CreateStates(const NormalBossData* data, NormalBossStateManager* sManeger, std::array<EnemyBullet, NormalBossData::kNormalBossBulletMax>* bullets);
+	std::unordered_map<uint8_t, std::unique_ptr<IEnemyState>> CreateStates(const NormalBossData* data, 
+		NormalBossStateManager* sManeger, std::array<EnemyBullet, NormalBossData::kNormalBossBulletMax>* bullets,
+		MinionManager* mManager);
 }
